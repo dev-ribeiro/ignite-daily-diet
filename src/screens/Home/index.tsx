021e471d5729b08f0f982958ec5a006077a0c61c
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-16 19:42:48 -0300

    fix: adjust list on home screen - implement section list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,56 @@
+import { SectionList, Text, View } from "react-native";
 import { CreateMeal } from "@components/CreateMeal";
-import { DailySummary } from "@components/DailySummary";
+import { FoodCard } from "@components/FoodCard";
 import { Header } from "@components/Header";
 import { PercentPanel } from "@components/PercentPanel";
-import { FlatList } from "react-native";
-import { Container } from "./styles";
+import { Container, DayHeader } from "./styles";
+
+type Food = {
+    food: string
+    hour: string
+    isDiet: boolean
+}
+
+type Section = {
+    title: string
+    data: Food[]
+}
 
 export function Home() {
-    const days: string[] = ['12/05/22', '13/05/22', '14/05/22', '16/05/22']
+    const data: Section[] = [
+        {
+            title: '12/03/22', data: [
+                { food: 'X-tudo', hour: '20:00', isDiet: false },
+                { food: 'X-salada', hour: '20:00', isDiet: false },
+            ]
+        },
+        { title: '13/03/22', data: [{ food: 'X-tudo', hour: '20:00', isDiet: true }] },
+        { title: '14/03/22', data: [{ food: 'X-tudo', hour: '20:00', isDiet: true }] },
+    ]
 
     return (
         <Container>
             <Header />
             <PercentPanel percent={5100} />
             <CreateMeal />
-            <FlatList
-                data={days}
-                keyExtractor={item => item}
+            <SectionList
+                sections={data}
+                keyExtractor={(item, index) => `${item.food}-${item.hour}=>${index}`}
+                renderSectionHeader={({ section: { title } }) => {
+                    return (
+                        <DayHeader
+                            style={{
+                                marginTop: (data[0].title === title) ? 0 : 32
+                            }}
+                        >{title}</DayHeader>
+                    )
+                }}
                 renderItem={({ item }) => (
-                    <DailySummary day={item} />
+                    <FoodCard
+                        food={item.food}
+                        hour={item.hour}
+                        indicator={!item.isDiet ? 'failure' : 'success'}
+                    />
                 )}
             />
         </Container>
