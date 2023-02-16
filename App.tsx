43d881786ed3b871d373b459a0b06ab104a70b03
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-15 21:15:23 -0300

    config:  implement font loaded

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import { Home } from '@screens/Home';
+import { StatusBar, Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from "@expo-google-fonts/nunito-sans"
 import defaultTheme from './src/theme/default'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <Text>Carregando...</Text>
+    )
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <>
-        <StatusBar style="auto" />
+        <StatusBar
+          backgroundColor="transparent"
+          barStyle="dark-content"
+        />
+        <Home />
       </>
     </ThemeProvider>
   );
