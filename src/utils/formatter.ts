74b3260d2e44093f32b6e5dd2958c5df288d22d6
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-17 17:44:08 -0300

    feat: create formatter functions to date and hour

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -5,4 +5,27 @@ function formatNumberToString(number: number): string {
     return String(parseNumberToPercent).replace(".", ",")
 }
 
-export { formatNumberToString }
+function formatZeroLeft(time: number) {
+
+    if (time < 10) {
+        return "0" + time
+    }
+
+    return String(time)
+}
+
+function formatDateToBRFormat(date: Date): string {
+
+    const formatedDate = `${formatZeroLeft(date.getDate())}/${formatZeroLeft(date.getMonth())}/${date.getFullYear()}`
+
+    return formatedDate
+}
+
+function formatHourToBRFormat(date: Date): string {
+    const hour = date.getUTCHours()
+    const minutes = date.getUTCMinutes()
+
+    return `${formatZeroLeft(hour)}:${formatZeroLeft(minutes)}`
+}
+
+export { formatNumberToString, formatDateToBRFormat, formatHourToBRFormat }
