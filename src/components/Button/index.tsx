62127b9f22385a8be94f4fd2d749f2d6d1256a5f
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-02-18 17:23:41 -0300

    feat: implement more variants in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import { TouchableOpacityProps } from "react-native"
 import { Separator } from "@components/Separator";
-import { ButtonVariantStyleType, Container, Icon, Indicator, Title } from "./styles";
+import { ButtonVariantStyleType, Container, Icon, Indicator, PencilIcon, Title, TrashIcon } from "./styles";
 
 type Props = TouchableOpacityProps & {
     isSelected?: boolean
@@ -18,6 +18,34 @@ export function Button({ isSelected = false, hasIcon = false, title, variant = "
                 {...props}
             >
                 {hasIcon && (<Icon />)}
+                <Title variant={variant}>{title}</Title>
+            </Container>
+        )
+    }
+
+    if (variant === "white") {
+        return (
+            <Container
+                variant={variant}
+                isSelected={isSelected}
+                {...props}
+            >
+                <TrashIcon />
+                <Separator width={12} />
+                <Title variant={variant}>{title}</Title>
+            </Container>
+        )
+    }
+
+    if (variant === "edit") {
+        return (
+            <Container
+                variant={variant}
+                isSelected={isSelected}
+                {...props}
+            >
+                <PencilIcon />
+                <Separator width={12} />
                 <Title variant={variant}>{title}</Title>
             </Container>
         )
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,8 +1,8 @@
 import { TouchableOpacity } from "react-native";
 import styled, { css } from "styled-components/native";
-import { Plus } from "phosphor-react-native"
+import { PencilSimpleLine, Plus, Trash } from "phosphor-react-native"
 
-export type ButtonVariantStyleType = "default" | "success" | "failure"
+export type ButtonVariantStyleType = "default" | "success" | "failure" | "edit" | "white"
 
 type Props = {
     variant: ButtonVariantStyleType
@@ -17,19 +17,28 @@ export const Container = styled(TouchableOpacity) <Props & { isSelected: boolean
     border-radius: 6px;
 
     ${({ isSelected, theme, variant }) => {
-
-
-        if(variant === "success" && isSelected === true){
+        if (variant === "success" && isSelected === true) {
             return css`
                 flex: 1;
                 background-color: ${theme.COLORS.GREEN_LIGHT};
             `
         }
 
-        if(variant === "failure" && isSelected === false){
+        if (variant === "failure" && isSelected === false) {
             return css`
                 flex: 1;
                 background-color: ${theme.COLORS.RED_LIGHT};
+            `
+        }
+
+        if (variant === "edit") {
+            return css``
+        }
+
+        if(variant === "white"){
+            return css`
+                background-color: ${theme.COLORS.WHITE};
+                border: 1px solid ${theme.COLORS.GRAY_100};
             `
         }
 
@@ -50,6 +59,18 @@ export const Title = styled.Text<Props>`
     `};
 
     ${({ theme, variant }) => {
+        if (variant === "edit") {
+            return css`
+                color: ${theme.COLORS.WHITE};
+            `
+        }
+
+        if(variant === "white"){
+            return css`
+                color: ${theme.COLORS.GRAY_100};
+            `
+        }
+
         if (variant !== "default") {
             return css`
                 color: ${theme.COLORS.GRAY_100};
@@ -83,3 +104,13 @@ export const Icon = styled(Plus).attrs(({ theme }) => ({
 }))`
     margin-right: 12px;
 `
+
+export const PencilIcon = styled(PencilSimpleLine).attrs(({ theme }) => ({
+    size: 24,
+    color: theme.COLORS.WHITE
+}))``
+
+export const TrashIcon = styled(Trash).attrs(({ theme }) => ({
+    size: 24,
+    color: theme.COLORS.GRAY_100
+}))``
